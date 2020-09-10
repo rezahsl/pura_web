cbb6e3e2aa8531bd32a197765543355d6f6a1b52
Reza Setiabekti <reza.hsl@kudus.puragroup.com>
2020-09-10 15:36:24 +0700

    Edited September 10th, 2020 at 15.30

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <title>Pura Seal and Protect</title>
+  </head>
+
+  <body>
+    @yield('content')
+
+    @section('sidebar')
+      <div class="sidebar">
+        <h3>Sidebar</h3>
+        This is the sidebar
+        @show
+      </div>
+
+  </body>
+</html>
